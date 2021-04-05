147d27a383f05d497a5ed30f7072b8ccc189dcb9
Cho Yeonwoo <wooisso@naver.com>
2021-04-06 02:30:19 +0900

    separate page contents into functions

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -5,9 +5,102 @@ import { LandingPagePage, Page1Desc, Page1Title, Page2Title, Page3Desc, Page3Tit
 import mockUpImage1 from './phone_mockup1.png'
 import mockUpImage2 from './phone_mockup2.png'
 
+function Page1Content() {
+  return (
+    <>
+      <Page1Title>
+        함께 나누는<br />친환경 소비 플랫폼
+      </Page1Title>
+      <Page1Desc>
+        &quot;지구를 위한 착한 소비, 내 주머니에도 착한 소비&quot;
+      </Page1Desc>
+    </>
+  )
+}
+
+function Page2Content() {
+  return (
+    <>
+      <svg
+        viewBox='0 0 402 368'
+        width={402}
+        height={368}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -10,
+        }}
+      >
+        <ellipse cx={124} cy={124} rx={201} ry={184} fill='#11A656' />
+      </svg>
+      <Page2Title>
+        <u>학교인증</u>으로<br />공유소비 신뢰도 보장
+      </Page2Title>
+      <img
+        src={mockUpImage1}
+        alt=''
+        width={334}
+        height={637}
+        style={{
+          width: Math.min((window.innerHeight - 200) * 334 / 637, 334),
+          height: Math.min(window.innerHeight - 200, 637),
+          display: 'block',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+        }}
+      />
+      <svg
+        viewBox='0 0 230 210'
+        width={230}
+        height={210}
+        style={{
+          position: 'absolute',
+          bottom: 36,
+          right: 0,
+          zIndex: -10,
+        }}
+      >
+        <ellipse cx={165} cy={105} rx={115} ry={105} fill='#11A656' />
+      </svg>
+    </>
+  )
+}
+
+function Page3Content() {
+  const history = useHistory()
+  return (
+    <>
+      <Page3Title>
+        나눠 살 사람만 모여!
+      </Page3Title>
+      <img
+        src={mockUpImage2}
+        alt=''
+        width={334}
+        height={637}
+        style={{
+          width: Math.min((window.innerHeight - 200) * 334 / 637, 334),
+          height: Math.min(window.innerHeight - 200, 637),
+          display: 'block',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+        }}
+      />
+      <Page3Desc>
+        기존의 중고거래 플랫폼과는 다른,<br /><u><b>신개념 공유소비 플랫폼</b></u>
+      </Page3Desc>
+      <StartBtn
+        onClick={() => history.replace('/')}
+      >
+        나누기 시작
+      </StartBtn>
+    </>
+  )
+}
+
 function LandingPage() {
   const [height, setHeight] = useState(window.innerHeight)
-  const history = useHistory()
 
   const layoutListener = () => {
     setHeight(window.innerHeight)
@@ -25,82 +118,13 @@ function LandingPage() {
   return (
     <LandingPagePage>
       <PageContainer height={height}>
-        <Page1Title>
-          함께 나누는<br />친환경 소비 플랫폼
-        </Page1Title>
-        <Page1Desc>
-          &quot;지구를 위한 착한 소비, 내 주머니에도 착한 소비&quot;
-        </Page1Desc>
+        <Page1Content />
       </PageContainer>
       <PageContainer height={height}>
-        <svg
-          viewBox='0 0 402 368'
-          width={402}
-          height={368}
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            zIndex: -10,
-          }}
-        >
-          <ellipse cx={124} cy={124} rx={201} ry={184} fill='#11A656' />
-        </svg>
-        <Page2Title>
-          <u>학교인증</u>으로<br />공유소비 신뢰도 보장
-        </Page2Title>
-        <img
-          src={mockUpImage1}
-          alt=''
-          width={334}
-          height={637}
-          style={{
-            width: Math.min((window.innerHeight - 200) * 334 / 637, 334),
-            height: Math.min(window.innerHeight - 200, 637),
-            display: 'block',
-            marginRight: 'auto',
-            marginLeft: 'auto',
-          }}
-        />
-        <svg
-          viewBox='0 0 230 210'
-          width={230}
-          height={210}
-          style={{
-            position: 'absolute',
-            bottom: 36,
-            right: 0,
-            zIndex: -10,
-          }}
-        >
-          <ellipse cx={165} cy={105} rx={115} ry={105} fill='#11A656' />
-        </svg>
+        <Page2Content />
       </PageContainer>
       <PageContainer height={height}>
-        <Page3Title>
-          나눠 살 사람만 모여!
-        </Page3Title>
-        <img
-          src={mockUpImage2}
-          alt=''
-          width={334}
-          height={637}
-          style={{
-            width: Math.min((window.innerHeight - 200) * 334 / 637, 334),
-            height: Math.min(window.innerHeight - 200, 637),
-            display: 'block',
-            marginRight: 'auto',
-            marginLeft: 'auto',
-          }}
-        />
-        <Page3Desc>
-          기존의 중고거래 플랫폼과는 다른,<br /><u><b>신개념 공유소비 플랫폼</b></u>
-        </Page3Desc>
-        <StartBtn
-          onClick={() => history.replace('/')}
-        >
-          나누기 시작
-        </StartBtn>
+        <Page3Content />
       </PageContainer>
     </LandingPagePage>
   )
