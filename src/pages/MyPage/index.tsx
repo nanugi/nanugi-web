b1685cd028c3f48201422adb1a499d668e9c5007
Cho Yeonwoo <wooisso@naver.com>
2021-04-14 17:21:48 +0900

    update mypage

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react';
-import { ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { observer } from 'mobx-react-lite';
-import ResignIcon from '@material-ui/icons/MeetingRoom';
-import { useHistory } from 'react-router-dom';
-import { userStore } from '../../container/user/store';
-import { resignUser } from '../../container/user';
-import NavigationBar from '../../components/NavigationBar';
+import React, { useEffect } from 'react'
+import { observer } from 'mobx-react-lite'
+import { useHistory } from 'react-router-dom'
+import { userStore } from '../../container/user/store'
+import NavigationBar from '../../components/NavigationBar'
 import {
   Divider,
   EmailText,
@@ -17,8 +14,8 @@ import {
   NeedSignInText,
   SignUpButton,
   WelcomeTitle,
-} from './style';
-import TopHeader from '../../components/TopHeader';
+} from './style'
+import TopHeader from '../../components/TopHeader'
 
 const MyPage = observer(() => {
   const history = useHistory();
@@ -65,38 +62,25 @@ const MyPage = observer(() => {
         </div>
       </MyInfoLayout>
       <Divider />
-      <ItemButtonRow>계정 정보 관리</ItemButtonRow>
+      <ItemButtonRow onClick={() => history.push('/mypage/edit')}>
+        계정 정보 관리
+      </ItemButtonRow>
       <Divider />
-      <ItemButtonRow>나의 나누기</ItemButtonRow>
+      <ItemButtonRow onClick={() => history.push('/mypage/posts')}>
+        나의 나누기
+      </ItemButtonRow>
       <Divider />
-      <ItemButtonRow>관심목록</ItemButtonRow>
+      <ItemButtonRow onClick={() => history.push('/mypage/favs')}>
+        관심목록
+      </ItemButtonRow>
       <Divider />
-      <ItemButtonRow onClick={() => history.push('/cs')}>FAQ</ItemButtonRow>
+      <ItemButtonRow onClick={() => history.push('/cs')}>
+        문의하기
+      </ItemButtonRow>
       <Divider />
       <ItemButtonRow onClick={() => history.push('/policies')}>
         이용약관
       </ItemButtonRow>
-      <ListItem
-        button
-        onClick={async () => {
-          if (confirm('정말 회원탈퇴하시겠습니까?')) {
-            try {
-              const res = await resignUser();
-              if (res?.success) {
-                alert('회원탈퇴하였습니다. 홈으로 돌아갑니다.');
-                history.replace('/');
-              }
-            } catch (e) {
-              alert(e);
-            }
-          }
-        }}
-      >
-        <ListItemIcon>
-          <ResignIcon />
-        </ListItemIcon>
-        <ListItemText primary="회원탈퇴" />
-      </ListItem>
       <NavigationBar currnetUrl="mypage" />
     </MyPagePage>
   );
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { ProfilePage, ProfileUserNameText } from './style'
-import TopHeader from "../../components/TopHeader";
-import { getUser } from "../../container/user";
+import TopHeader from '../../components/TopHeader'
+import { getUser } from '../../container/user'
 
 function Profile() {
   const { nickname: userName } = useParams<{ nickname: string }>()
