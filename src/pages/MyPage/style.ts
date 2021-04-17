c6da02019ddbccdc779c874277eec348e831481a
outsung <insung9546@gmail.com>
2021-04-17 22:23:59 +0900

    fix scroll in mypage
    스크롤 버그 수정

--- src/pages/MyPage/style.ts
+++ src/pages/MyPage/style.ts
@@ -1,15 +1,22 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const MyPagePage = styled.div`
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-`
+  width: 100%;
+  height: 100%;
+
+  position: fixed;
+  overflow: hidden;
+`;
+export const MyPageContainer = styled.div`
+  height: calc(100% - 60px);
+  overflow-y: scroll;
+`;
 
 export const MyInfoLayout = styled.div`
-  width: 100%;
   padding: 30px 27px 25px;
-`
+  width: 100%;
+  box-sizing: border-box;
+`;
 
 export const LoginButton = styled.button`
   border-width: 1px;
@@ -17,7 +24,7 @@ export const LoginButton = styled.button`
   height: 30px;
   width: 80px;
   background-color: white;
-  border-color: #11A656;
+  border-color: #11a656;
   outline: none;
   font-size: 11px;
   color: black;
@@ -26,7 +33,7 @@ export const LoginButton = styled.button`
   margin-right: 8px;
   font-family: Noto Sans KR;
   font-weight: 500;
-`
+`;
 
 export const LogoutButton = styled.button`
   border-width: 1px;
@@ -34,7 +41,7 @@ export const LogoutButton = styled.button`
   height: 30px;
   width: 80px;
   background-color: white;
-  border-color: #11A656;
+  border-color: #11a656;
   outline: none;
   font-size: 11px;
   color: black;
@@ -43,12 +50,12 @@ export const LogoutButton = styled.button`
   margin-right: 8px;
   font-family: Noto Sans KR;
   font-weight: 500;
-`
+`;
 
 export const SignUpButton = styled.button`
   height: 30px;
   width: 80px;
-  background-color: #11A656;
+  background-color: #11a656;
   font-size: 11px;
   color: white;
   border: none;
@@ -56,34 +63,34 @@ export const SignUpButton = styled.button`
   text-align: center;
   font-family: Noto Sans KR;
   line-height: 30px;
-`
+`;
 
 export const WelcomeTitle = styled.div`
   font-size: 28px;
   color: black;
   font-family: Noto Sans KR;
   margin-bottom: 4px;
-`
+`;
 
 export const NeedSignInText = styled.div`
   font-size: 21px;
   color: black;
   font-family: Noto Sans KR;
   margin-bottom: 4px;
-`
+`;
 
 export const EmailText = styled.div`
   font-size: 14px;
-  color: #BDBDBD;
+  color: #bdbdbd;
   font-family: Noto Sans KR;
   margin-bottom: 16px;
-`
+`;
 
 export const Divider = styled.div`
   height: 10px;
-  background-color: #F2F2F2;
+  background-color: #f2f2f2;
   width: 100%;
-`
+`;
 
 export const ItemButtonRow = styled.button`
   display: block;
@@ -99,9 +106,9 @@ export const ItemButtonRow = styled.button`
   background-color: white;
   text-align: left;
   opacity: 1;
-  
+
   &:active {
-    background-color: #F2F2F2;
+    background-color: #f2f2f2;
     opacity: 0.38;
   }
-`
+`;
